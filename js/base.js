9372b23080ca9f458fdefb693df22eed37ca163e
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2021-07-18 02:12:48 +0600

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -1,10 +1,10 @@
 /* ==========================
-:: JS Name: Disshit Code
+:: JS Name: Disshit Base
 :: Author: NaeemBolchhi
-:: License: GPL-2.0 License
+:: License: GPL-3.0 License
 :: Landing Page: https://naeembolchhi.github.io/
 :: Telegram Profile: https://t.me/NaeemBolchhi
-:: Description: Helps in encoding/decoding strings to/from Base64.
+:: Description: Contains JavaScript and JQuery for Disshit.
 :: Version: v1.0
 :: Created: 16/07/2021
 ========================== */
@@ -22,4 +22,17 @@ function decodeDisshit(str) {
   return decodeURIComponent(atob(str).split('').map(function (c) {
     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
+}
+
+// Layer Switching
+function layerSwitch() {
+  var layer0 = document.getElementById('bottom');
+  var layer1 = document.getElementById('top');
+  if (layer0.style.display === "none") {
+	layer0.style.display = "flex";
+	layer1.style.display = "none";
+  } else {
+	layer1.style.display = "flex";
+	layer0.style.display = "none";
+  }
 }
